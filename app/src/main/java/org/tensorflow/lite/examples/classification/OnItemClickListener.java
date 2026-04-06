2d30936cf4089a1657168e7ca79f150464f4f18e
정혜주 <jm30806@naver.com>
2026-04-06 13:47:11 +0900

    refactor: 앱 패키지명을 com.recymera로 변경

--- app/src/main/java/org/tensorflow/lite/examples/classification/OnItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package org.tensorflow.lite.examples.classification;
-
-public interface OnItemClickListener {
-    String onItemClick (String name);
-    void onSortItemClick(int pos);
-    void onResultItemClick (int pos);
-}
